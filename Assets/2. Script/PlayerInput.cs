0371a6715434d2db78829ed68125dfe9aaa78484
HyeonSooLim <parisrice85@gmail.com>
2024-11-30 01:03:15 +0900

    [기능] 플레이어 인풋키를 인스펙터에서 설정
    1. 플레이어 인풋키를 인스펙터에서 할당할 수 있도록 변수 조정
    2. 외부에서 키를 바꿀 수 있도록 SetKeyMapping 함수 추가

--- Assets/2. Script/PlayerInput.cs
+++ Assets/2. Script/PlayerInput.cs
@@ -1,16 +1,81 @@
 using UnityEngine;
 
+public enum PlayerAction
+{
+    HorizontalPositive,
+    HorizontalNegative,
+    VerticalPositive,
+    VerticalNegative,
+    Jump
+}
+
 public class PlayerInput : MonoBehaviour
 {
     public float Horizontal { get; private set; }
     public float Vertical { get; private set; }
     public bool Jump { get; private set; }
+    public bool CameraRotate { get; private set; }
 
-    // Update is called once per frame
+    [SerializeField] private KeyCode horizontalPositiveKey = KeyCode.D;
+    [SerializeField] private KeyCode horizontalNegativeKey = KeyCode.A;
+    [SerializeField] private KeyCode verticalPositiveKey = KeyCode.W;
+    [SerializeField] private KeyCode verticalNegativeKey = KeyCode.S;
+    [SerializeField] private KeyCode jumpKey = KeyCode.Space;
+    [SerializeField] private KeyCode cameraRotationKey = KeyCode.Mouse1;
+
+    void Start()
+    {
+        if (TryGetComponent<CameraController>(out CameraController controller) == false)
+        {
+            gameObject.AddComponent<CameraController>();
+        }
+    }
+
     void Update()
     {
-        Horizontal = Input.GetAxis("Horizontal");
-        Vertical = Input.GetAxis("Vertical");
-        Jump = Input.GetButtonDown("Jump");
+        Horizontal = 0;
+        Vertical = 0;
+
+        if (Input.GetKey(horizontalPositiveKey))
+        {
+            Horizontal += 1;
+        }
+        if (Input.GetKey(horizontalNegativeKey))
+        {
+            Horizontal -= 1;
+        }
+        if (Input.GetKey(verticalPositiveKey))
+        {
+            Vertical += 1;
+        }
+        if (Input.GetKey(verticalNegativeKey))
+        {
+            Vertical -= 1;
+        }
+
+        CameraRotate = Input.GetKey(cameraRotationKey);
+        Jump = Input.GetKeyDown(jumpKey);
+    }
+
+    public void SetKeyMapping(PlayerAction action, KeyCode newKey)
+    {
+        switch (action)
+        {
+            case PlayerAction.HorizontalPositive:
+                horizontalPositiveKey = newKey;
+                break;
+            case PlayerAction.HorizontalNegative:
+                horizontalNegativeKey = newKey;
+                break;
+            case PlayerAction.VerticalPositive:
+                verticalPositiveKey = newKey;
+                break;
+            case PlayerAction.VerticalNegative:
+                verticalNegativeKey = newKey;
+                break;
+            case PlayerAction.Jump:
+                jumpKey = newKey;
+                break;
+        }
     }
 }
